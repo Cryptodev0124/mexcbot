07b855bd794699d6f574d1df9465a21a918df846
Cryptodev0124 <cryptodev0124@gmail.com>
2024-08-07 19:04:18 +1000

    updated

--- future.js
+++ future.js
@@ -25,8 +25,9 @@ app.get('/', (req, res) => {
 });
 app.post('/exchange', async (req, res) => {
   const { symbol, side, quantity, authorization, api_Key, secret_Key, leverage } = req.body;
-  if (side === 100) {
-    side = side * 98 / 100;
+  console.log("here");
+  if (quantity === 100) {
+    quantity = quantity * 98 / 100;
   }
   console.log("values", symbol, side, quantity, authorization, api_Key, secret_Key, leverage);
   const apiKey = process.env.API_KEY || api_Key;
@@ -69,23 +70,64 @@ app.post('/exchange', async (req, res) => {
 
     console.log("quantity", quantity, orderAmount);
 
-    if (orderAmount >= maxAmount) {
-      BASE_AMOUNT = maxAmount;
-    } else {
-      BASE_AMOUNT = orderAmount;
+    const result = await readClient.OpenPositions();
+    if ((side == 1) || (side == 3)) {
+      console.log("longPosition");
+      if (orderAmount >= maxAmount) {
+        console.log("base1");
+        BASE_AMOUNT = maxAmount;
+      } else {
+        console.log("base2");
+        BASE_AMOUNT = orderAmount;
+      }
+    } else if ((side == 2) || (side == 4)) {
+      console.log("shortPosition");
+      if (result.data.data) {
+        console.log("base3", (result.data.data[0].holdVol * quantity / 100).toFixed(1));
+        BASE_AMOUNT = (result.data.data[0].holdVol * quantity / 100).toFixed(1);
+      } else {
+        console.log("base4");
+        BASE_AMOUNT = 0;
+      }
     }
+    console.log("baseAmount", side, orderAmount, BASE_AMOUNT);
     if (!isReady) return;
     isReady = false;
-    const result = await readClient.OpenPositions();
     var action;
     var action_tag = 0;
     console.log("success", result.data);
     if (result.data.success) {
-      action = await client.PlaceNewOrder({
-        ...DEFAULT_OPTION,
-        side: side,
-        vol: ((BASE_AMOUNT / contractSize)).toFixed(0)
-      });
+      if ((side == 1) || (side == 3)) {
+        action = await client.PlaceNewOrder({
+          ...DEFAULT_OPTION,
+          side: side,
+          vol: ((BASE_AMOUNT / contractSize)).toFixed(0)
+        });
+      } else if (side == 4) {
+        action = await client.PlaceNewOrder({
+          ...DEFAULT_OPTION,
+          side: 4,
+          vol: ((BASE_AMOUNT / contractSize)).toFixed(0)
+        });
+        action = await client.PlaceNewOrder({
+          ...DEFAULT_OPTION,
+          side: 1,
+          vol: (((result.data.data[0].holdVol * (100 - quantity) / 100).toFixed(1) / contractSize)).toFixed(0)
+        });
+        action_tag = 1;
+      } else if (side == 2) {
+        action = await client.PlaceNewOrder({
+          ...DEFAULT_OPTION,
+          side: 2,
+          vol: ((BASE_AMOUNT / contractSize)).toFixed(0)
+        });
+        action = await client.PlaceNewOrder({
+          ...DEFAULT_OPTION,
+          side: 3,
+          vol: (((result.data.data[0].holdVol * (100 - quantity) / 100).toFixed(1) / contractSize)).toFixed(0)
+        });
+        action_tag = 2;
+      }
       action_tag = 1;
       console.log('orderAmount', ((BASE_AMOUNT / contractSize)).toFixed(0))
       console.log("Created successful!", action.data);
@@ -184,43 +226,69 @@ app.post('/reverse', async (req, res) => {
   reverse();
 });
 
-// app.post('/reverse', async (req, res) => {
-//   console.log("here");
-//   const { authorization, api_Key, secret_Key } = req.body;
+app.post('/sellAll', async (req, res) => {
+  const { authorization, api_Key, secret_Key } = req.body;
 
-//   const apiKey = process.env.API_KEY || api_Key;
-//   const apiSecret = process.env.SECRET_KEY || secret_Key;
-//   const baseUrl = 'https://futures.mexc.com';
-//   const client = new Alternative(process.env.AUTHORIZATION || authorization, apiSecret, { baseURL: baseUrl, isAlternative: true })
-//   const readClient = new Future(apiKey, apiSecret, { baseURL: 'https://contract.mexc.com' });
+  const apiKey = process.env.API_KEY || api_Key;
+  const apiSecret = process.env.SECRET_KEY || secret_Key;
+  const baseUrl = 'https://futures.mexc.com';
+  const client = new Alternative(process.env.AUTHORIZATION || authorization, apiSecret, { baseURL: baseUrl, isAlternative: true })
+  const readClient = new Future(apiKey, apiSecret, { baseURL: 'https://contract.mexc.com' });
 
-//   var isReady = true;
+  var isReady = true;
 
-//   const reverse = async () => {
-//     if (!isReady) return;
-//     isReady = false;
-//     const result = await readClient.OpenPositions();
-//     console.log("openPosition", result);
-//     var action;
-//     var action_tag = 0;
-//     console.log("success", result.data);
-//     if (result.data.success) {
-//       action = await client.Reverse({
-//         positionId: result.data.data[0].positionId,
-//         symbol: result.data.data[0].symbol,
-//         positionType: result.data.data[0].positionType,
-//         state: result.data.data[0].state,
-//         leverage: result.data.data[0].leverage,
-//       });
-//       action_tag = 1;
-//       console.log("Reverse successful!", action.data)
-//       res.send(action.data)
-//     }
-//     isReady = true;
-//   }
-//   info = readFromStorage('info');
-//   reverse();
-// });
+  const sellAll = async () => {
+    if (!isReady) return;
+    isReady = false;
+    const result = await readClient.OpenPositions();
+    console.log("resultPosition", result.data);
+    var action;
+    var action_tag = 0;
+    const symbol = result.data.data[0].symbol;
+    var sideVol = result.data.data[0].positionType;
+    const volume = result.data.data[0].holdVol;
+    const leverage = result.data.data[0].leverage;
+    
+    console.log("getPositionResult", symbol, sideVol, volume, leverage);
+    var MARKET_SYMBOL = symbol;
+    const LEAVERAGE_MULTIPLIER = leverage;
+    const DEFAULT_OPTION = {
+      symbol: MARKET_SYMBOL,
+      openType: 2, // Cross Margin
+      type: "5", // Market Order
+      leverage: LEAVERAGE_MULTIPLIER,
+      marketCeiling: false,
+      priceProtect: "0",
+      reduceOnly: true
+    }
+    if (result.data.success) {
+      if (sideVol === 1) {
+        action = await client.PlaceNewOrder({
+          ...DEFAULT_OPTION,
+          side: 4,
+          vol: volume
+        });
+        action_tag = 1;
+        console.log("Closed successful!", action.data);
+        res.send(action.data)
+      } else if (sideVol === 2) {
+        action = await client.PlaceNewOrder({
+          ...DEFAULT_OPTION,
+          side: 2,
+          vol: volume
+        });
+        action_tag = 1;
+        console.log("Closed successful!", action.data);
+        res.send(action.data)
+      }
+      // action = await client.CancelAll();
+      console.log("Closed successful!", action.data);
+    }
+    isReady = true;
+  }
+  info = readFromStorage('info');
+  sellAll();
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
